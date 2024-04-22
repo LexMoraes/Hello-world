f725d2678b337129ba80a31bdd8ce4a5af4aa50f
LexMoraes <154477172+LexMoraes@users.noreply.github.com>
2024-04-22 20:57:43 +0000

    Exercícios estrutura sequencial - PythonBrasil

---
+++ Exercicios_algoritmos_python/ex009-conversor_termométrico.py
@@ -0,0 +1,6 @@
+print("Este é o programa de conversão de temperatura Fahrenheit para Celsius.")
+fahrenheit = float(input("Digite o valor da temperatura a ser convertida: "))
+
+celsius = 5 * ((fahrenheit-32) / 9)
+
+print(f"A temperatura {fahrenheit}°F em Celsius é: {celsius}°C")
