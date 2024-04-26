1ea2ca47f0858ff80b5f68eef1f538897bdd0654
LexMoraes <154477172+LexMoraes@users.noreply.github.com>
2024-04-26 16:50:43 -0400

    Update ex103-listas_impar_par.py
    Melhorando validações

--- Exercicios_algoritmos_python/ex103-listas_impar_par.py
+++ Exercicios_algoritmos_python/ex103-listas_impar_par.py
@@ -5,23 +5,28 @@
 def ler_numeros():
     numeros = []
     for c in range(20):
-        numero = int(input(f'Digite {c+1}º numero inteiro: '))
+        numero = int(input(f'Digite o {c+1}º numero inteiro: '))
         numeros.append(numero)
     return numeros
 
 def impar_par(numeros):
     impar = []
     par = []
+    nulos = 0
+
     for n in numeros:
-        if n % 2 == 0:
+        if n % 2 == 0 and n != 0:
             par.append(n)
+        elif n == 0:
+            nulos += 1
         else:
             impar.append(n)
-    return impar, par
+    return impar, par, nulos
 
 numeros = ler_numeros()
 
-impar, par = impar_par(numeros)
+impar, par, nulos = impar_par(numeros)
 
 print('impar\n',impar)
-print('par\n', par)
+print('par:\n', par)
+print('Dígitos nulos: \n', nulos)
