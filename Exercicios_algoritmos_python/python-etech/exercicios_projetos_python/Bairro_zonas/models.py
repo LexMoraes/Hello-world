6a5224029ce418a1e8d68830240f90411f08ada6
LexMoraes <154477172+LexMoraes@users.noreply.github.com>
2024-05-01 15:36:48 -0400

    Atualizando formatação.py

--- Exercicios_algoritmos_python/python-etech/exercicios_projetos_python/Bairro_zonas/models.py
+++ Exercicios_algoritmos_python/python-etech/exercicios_projetos_python/Bairro_zonas/models.py
@@ -10,12 +10,15 @@
 class Bairro:
     id = 1
 
-    def __init__(self, nome=None, zona=None, populacao=None):
+    def __init__(self, nome, zona, populacao):
         self.codigo = Bairro.id
         self.nome = nome
         self.zona = zona
         self.populacao = populacao
 
+    def __str__(self):
+        return f'{self.codigo}  |  {self.nome}  |  {self.zona}  |  {self.populacao}'
+
     @staticmethod
-    def incremento_id():
+    def incrementar_id():
         Bairro.id += 1
